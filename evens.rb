810fa1c89a52e78e65108fdfe0f3e8648074b0ee
Bri Dotson <bridotson@gmail.com>
2014-10-02 09:24:35 -0700

    add two methods for returning even elements

---
+++ evens.rb
@@ -0,0 +1,21 @@
+class EvenInds
+  def initialize(array)
+    @array = array
+    @even_inds = []
+  end
+
+  def method1
+    @even_inds = []
+    @array.each_with_index { |element, i| @even_inds << element if i % 2 == 0}
+  end
+
+  def method2
+    @even_inds = []
+    (0..@array.length-1).step(2) { |i| @even_inds << @array[i] }
+  end
+
+  def method3
+    
+  end
+
+end
